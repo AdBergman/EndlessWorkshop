5f3c7b9cf79409c48955effb5756594f3a59451c
AdBergman <bergman.adrian@gmail.com>
2025-09-16 11:16:08 +0200

    Initial commit: project skeleton setup, Maven, JDK, Git setup

---
+++ src/main/java/com/endlessworkshop/ewbackend/EndlessWorkshopApplication.java
@@ -0,0 +1,13 @@
+package com.endlessworkshop.ewbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EndlessWorkshopApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EndlessWorkshopApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/endlessworkshop/ewbackend/EndlessWorkshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.endlessworkshop.ewbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndlessWorkshopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
