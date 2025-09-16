4a946150a8897fa8363e522e92563feb3a90d27b
AdBergman <bergman.adrian@gmail.com>
2025-09-17 00:13:50 +0200

    Add tech_era_1 data, UI overlay coords, component, and graphics

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import TechTree from "./components/TechTreeEra1";
 
 function App() {
     return (
         <div className="App">
-            <h1>Endless Workshop</h1>
+            {/*<h1>Endless Workshop</h1>*/}
+            <TechTree />
         </div>
     );
 }
