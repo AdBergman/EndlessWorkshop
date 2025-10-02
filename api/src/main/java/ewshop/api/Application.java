4912ad26e3d12b02cf38fa788889bd2ee7427ea3
AdBergman <bergman.adrian@gmail.com>
2025-10-03 00:04:29 +0200

    Added Skeleton for Modern Multi-Module Hexagonal-Architecture Spring-Boot project. Confirmed that it is running.

--- api/src/main/java/ewshop/api/Application.java
+++ api/src/main/java/ewshop/api/Application.java
@@ -1,13 +1,13 @@
-package com.endlessworkshop.ewbackend;
+package ewshop.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class EndlessWorkshopApplication {
+@SpringBootApplication(scanBasePackages = "ewshop")
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(EndlessWorkshopApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
