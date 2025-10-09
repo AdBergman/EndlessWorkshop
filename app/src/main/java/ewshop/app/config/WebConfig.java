d8e315d95c28b66e9e7d984c39dc9068930e4eb1
AdBergman <bergman.adrian@gmail.com>
2025-10-09 22:19:35 +0200

    Update: Cache on Index.html set to 0 during development. Keeping cache on images to 30 days.

--- app/src/main/java/ewshop/app/config/WebConfig.java
+++ app/src/main/java/ewshop/app/config/WebConfig.java
@@ -2,20 +2,38 @@ package ewshop.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig {
 
     @Bean
-    public WebMvcConfigurer corsConfigurer() {
+    public WebMvcConfigurer webConfigurer() {
         return new WebMvcConfigurer() {
+
             @Override
             public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/api/**") // apply to your API endpoints
+                registry.addMapping("/api/**")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                // Disable cache for index.html
+                registry.addResourceHandler("/index.html")
+                        .addResourceLocations("classpath:/static/index.html")
+                        .setCacheControl(CacheControl.noCache());
+
+                // Keep 30 days for other static assets
+                registry.addResourceHandler("/**")
+                        .addResourceLocations("classpath:/static/")
+                        .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS));
             }
         };
     }
