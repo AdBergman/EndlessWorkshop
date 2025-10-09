b461f3acd2e4e688921333a30b625fd359b8d517
AdBergman <bergman.adrian@gmail.com>
2025-10-09 22:47:46 +0200

    Fix: Spring Properties are global and override the config. Removed properties. Removed Filter. Added WebConfig.

--- app/src/main/java/ewshop/app/config/WebConfig.java
+++ app/src/main/java/ewshop/app/config/WebConfig.java
@@ -2,8 +2,12 @@ package ewshop.app.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 @Configuration
 public class WebConfig {
@@ -17,6 +21,19 @@ public class WebConfig {
                 registry.addMapping("/api/**")
                         .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+            }
+
+            @Override
+            public void addResourceHandlers(ResourceHandlerRegistry registry) {
+                // Disable cache for index.html
+                registry.addResourceHandler("/index.html")
+                        .addResourceLocations("classpath:/static/index.html")
+                        .setCacheControl(CacheControl.noCache());
+
+                // Keep 30 days cache for all other static assets
+                registry.addResourceHandler("/**")
+                        .addResourceLocations("classpath:/static/")
+                        .setCacheControl(CacheControl.maxAge(30, TimeUnit.DAYS));
             }
         };
     }
