e2307d4be8a4365d50e77910705443a1e3b6f2fe
AdBergman <bergman.adrian@gmail.com>
2026-02-21 22:38:14 +0100

    Forcing Re-deploy.

--- frontend/src/components/GameSummary/views/SummaryLoadView.tsx
+++ frontend/src/components/GameSummary/views/SummaryLoadView.tsx
@@ -4,6 +4,7 @@ import "../CityBreakdown.css";
 import { loadEndGameReportFromText } from "@/features/endGame/import/endGameReportLoader";
 
 const EXAMPLE_PATH = "/EL2_EndGame_20260221_090536.json";
+
 const MOD_URL = "https://github.com/AdBergman/EL2StatsMod";
 
 const EXPORT_PATH =
