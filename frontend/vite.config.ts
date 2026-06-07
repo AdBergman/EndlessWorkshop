8907bafd01ec2bd5d6bf51ad66132d420e12bd61
AdBergman <bergman.adrian@gmail.com>
2026-06-08 00:03:02 +0200

    chore(vite): upgrade react plugin

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,14 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tsconfigPaths from 'vite-tsconfig-paths';
 import { resolve } from 'path';
 
 export default defineConfig(({ mode }) => {
     const isProduction = mode === 'production';
 
     return {
-        plugins: [tsconfigPaths(), react()],
+        plugins: [react()],
         resolve: {
+            tsconfigPaths: true,
             alias: {
                 '@': resolve(__dirname, 'src'),
             },
